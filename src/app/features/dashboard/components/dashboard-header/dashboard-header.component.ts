fde59ac8d09c8997f59cbc715b6d77488815e683
safinghoghabori <safinghoghabori65@gmail.com>
2024-10-01 00:24:37 +0530

    fix dashboard bugs

--- src/app/features/dashboard/components/dashboard-header/dashboard-header.component.ts
+++ src/app/features/dashboard/components/dashboard-header/dashboard-header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {
   TerminalData,
+  TerminalResp,
   TrCompanyData,
+  TrCompanyResp,
   UserType,
 } from '../../../../auth/login/models/login.model';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
@@ -13,22 +15,25 @@ import { AuthService } from '../../../../core/services/auth.service';
   standalone: true,
   templateUrl: './dashboard-header.component.html',
   styleUrl: './dashboard-header.component.css',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
 })
 export class DashboardHeaderComponent {
   userType: string | null = '';
   userData: TrCompanyData | TerminalData | undefined;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router, private authService: AuthService) {
+    this.userData = this.authService.getUserData();
+    this.userType = this.authService.getUserType();
+  }
 
   getUserName(): string {
     return this.userType === UserType.Terminal
-      ? (this.userData as TerminalData).portName
-      : (this.userData as TrCompanyData).trCompanyName;
+      ? (this.userData as TerminalResp).portName
+      : (this.userData as TrCompanyResp).trCompanyName;
   }
 
   addNewAppointment() {
-    this.router.navigate(['/new-appointment']);
+    this.router.navigate(['dashboard/new-appointment']);
   }
 
   logout() {
